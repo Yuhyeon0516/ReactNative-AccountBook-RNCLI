69baf848d0de975a5372898fa7fc2a3f7aa6707a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-07 14:58:51 +0900

    create convertToDateString function

---
+++ src/utils/DataUtil.ts
@@ -0,0 +1,16 @@
+export function convertToDateString(time: number): string {
+  const date = new Date(time);
+
+  function addZeroIfOneCharecter(number: number): string {
+    if (number < 10) {
+      return `0${number}`;
+    }
+    return number.toString();
+  }
+
+  return `${date.getFullYear()}-${addZeroIfOneCharecter(
+    date.getMonth() + 1,
+  )}-${addZeroIfOneCharecter(
+    date.getDate(),
+  )} ${date.getHours()}:${date.getMinutes()}`;
+}
