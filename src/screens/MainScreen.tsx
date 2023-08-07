4078ff2c9f3a74e8a6deeada81f31ff4b797627b
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-07 14:59:54 +0900

    create add floating button in main screen

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -4,11 +4,15 @@ import {Header} from '../components/Header/Header';
 import {AccountBookHistory} from '../type/AccountBookHistory';
 import AccountHistoryList from '../components/AccountHistoryList';
 import {useRootNavigation} from '../navigations/RootNavigation';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {CustomButton} from '../components/CustomButton';
+import {Icon} from '../components/Icons';
 
 const now = new Date().getTime();
 
 export default function MainScreen() {
-  const navigation = useRootNavigation<'Main'>();
+  const navigation = useRootNavigation();
+  const safeAreaInset = useSafeAreaInsets();
   const [list, setList] = useState<AccountBookHistory[]>([
     {
       id: 0,
@@ -30,6 +34,7 @@ export default function MainScreen() {
         'https://docs.expo.dev/static/images/tutorial/background-image.png',
     },
   ]);
+
   return (
     <View style={{flex: 1}}>
       <Header>
@@ -48,6 +53,29 @@ export default function MainScreen() {
           );
         }}
       />
+      <View
+        style={{
+          position: 'absolute',
+          right: 12,
+          bottom: 12 + safeAreaInset.bottom,
+        }}>
+        <CustomButton
+          onPress={() => {
+            navigation.push('Add');
+          }}>
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+              backgroundColor: 'red',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Icon iconName="add" size={30} color="white" />
+          </View>
+        </CustomButton>
+      </View>
     </View>
   );
 }
