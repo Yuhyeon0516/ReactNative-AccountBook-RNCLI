a6925bce360bde0ea1417f94399b63815e3ef2b4
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-07 16:33:49 +0900

    Revert "add close function using navigation.goBack()"
    This reverts commit 61622d035f626f79fa2248b7d0e97d94bede2e9c.

--- src/screens/AddUpdateScreen.tsx
+++ src/screens/AddUpdateScreen.tsx
@@ -1,20 +1,13 @@
 import {View} from 'react-native';
 import React from 'react';
 import {Header} from '../components/Header/Header';
-import {useRootNavigation} from '../navigations/RootNavigation';
 
 export default function AddUpdateScreen() {
-  const navigation = useRootNavigation();
   return (
     <View style={{flex: 1}}>
       <Header>
         <Header.Title title="Add/Update Screen" />
-        <Header.Icon
-          iconName="close"
-          onPress={() => {
-            navigation.goBack();
-          }}
-        />
+        <Header.Icon iconName="close" onPress={() => {}} />
       </Header>
     </View>
   );
